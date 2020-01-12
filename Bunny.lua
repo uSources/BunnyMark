ba0ef88133a80c20e850b9e2982146762ad3e189
uSources <hate463@gmail.com>
2020-01-12 16:01:07 +0100

    feat: bunny random spawn direction

--- Bunny.lua
+++ Bunny.lua
@@ -1,6 +1,7 @@
 require "assets"
 Bunny = {}
 Bunny.__index = Bunny
+local DIRECTIONS = {-1,1}
 
 function Bunny:create(x, y, speed)
     local o = {}
@@ -13,8 +14,8 @@ function Bunny:create(x, y, speed)
     o.color.g = math.random(0, 1)
     o.color.b = math.random(0, 1)
     o.color.a = 1
-    o.dirx = math.random(-1, 1)
-    o.diry = math.random(-1, 1)
+    o.dirx = math.random() * DIRECTIONS[math.random(1,2)]
+    o.diry = math.random() * DIRECTIONS[math.random(1,2)]
     return o
 end
 
@@ -22,20 +23,12 @@ function Bunny:draw()
 end
 
 function Bunny:update(dt)
-    if self.x < 0 then
-        self.dirx = 1
+    if self.x < 0 or self.x > love.graphics.getWidth() - bunny_sprite:getWidth() then
+        self.dirx = self.dirx * -1
     end
-    if self.x > love.graphics.getWidth() - bunny_sprite:getWidth() then
-        self.dirx = -1
+    if self.y < 0 or self.y > love.graphics.getHeight() - bunny_sprite:getHeight() then
+        self.diry = self.diry * -1
     end
-
-    if self.y < 0 then
-        self.diry = 1
-    end
-    if self.y > love.graphics.getHeight() - bunny_sprite:getHeight() then
-        self.diry = -1
-    end
-
     self.x = self.x + (self.dirx * self.speed * dt)
     self.y = self.y + (self.diry * self.speed * dt)
 end
