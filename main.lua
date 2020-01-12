c0318520134c05c6ee72287777860148fa89dafa
uSources <hate463@gmail.com>
2020-01-12 16:00:55 +0100

    feat: change bg color

--- main.lua
+++ main.lua
@@ -2,6 +2,7 @@ require "assets"
 
 local MAX_BUNNYES = 8000
 local BUNNY_COUNT = 0
+local BUNNY_SPANW = 1000
 local Bunny = require "Bunny"
 local Pool = {}
 local stats = love.graphics.getStats()
@@ -10,12 +11,12 @@ local batch = love.graphics.newSpriteBatch(bunny_sprite)
 function love.load()
 end
 function love.draw()
+  love.graphics.setBackgroundColor(1,1,1,1)
   love.graphics.draw(batch, x, y)
   love.graphics.print("Bunnies:" .. #Pool, 10, 10)
   love.graphics.print("FPS:" .. love.timer.getFPS(), 10, 25)
   love.graphics.print("MEMORY:" .. stats.texturememory, 10, 40)
   love.graphics.print("CALLBACKS:" .. stats.drawcalls, 10, 55)
-  print(#Pool)
 end
 
 function love.update(dt)
@@ -29,7 +30,7 @@ end
 
 function love.mousepressed(x, y, button, istouch)
   if button == 1 and MAX_BUNNYES > BUNNY_COUNT then
-    for i = 1, 100 do
+    for i = 1, BUNNY_SPANW do
       BUNNY_COUNT = BUNNY_COUNT + 1
       local x, y = love.mouse.getPosition()
       table.insert(Pool, Bunny:create(x, y, 300))
